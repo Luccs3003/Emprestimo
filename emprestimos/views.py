57309c4fc00431a5833854cf2fe6dc89e55a7da4
Lucas Cardoso <lucascardoso4283@gmail.com>
2024-06-09 19:56:03 -0300

    facade

--- emprestimos/views.py
+++ emprestimos/views.py
@@ -14,8 +14,8 @@ def lista_emprestimos(request):
 
 def adicionar_emprestimo(request):
     # Chama a função para obter e salvar os livros quando o formulário for renderizado
-    get_and_save_livros_from_api()
-    get_and_save_users_from_api()
+    IntegracaoAPIFacade.obter_e_salvar_livros()
+    IntegracaoAPIFacade.obter_e_salvar_usuarios()
 
     if request.method == "POST":
         form = EmprestimoForm(request.POST)
@@ -27,59 +27,6 @@ def adicionar_emprestimo(request):
     
     context = {'form': form}
     return render(request, 'emprestimos/adicionar_emprestimo.html', context)
-
-
-def get_and_save_users_from_api():
-    try:
-        response = requests.get('https://cadastrocliente-production.up.railway.app/api/usuarios')
-        response.raise_for_status()  # Verifica se houve algum erro na requisição
-
-        users_json = response.json()  # Converte a resposta para JSON
-
-        # Para cada usuário na resposta da API
-        for user_data in users_json:
-            cpf = user_data.get('cpf')  # Obtenha o CPF do usuário da API
-            # Verifique se o usuário já existe no banco de dados pelo CPF
-            existing_user = Usuario.objects.filter(cpf=cpf).first()
-            if existing_user:
-                # Se o usuário já existir, atualize os detalhes do usuário
-                serializer = UsuarioSerializer(existing_user, data=user_data)
-            else:
-                # Se o usuário não existir, crie um novo registro
-                serializer = UsuarioSerializer(data=user_data)
-
-            if serializer.is_valid():
-                serializer.save()
-    except requests.RequestException as e:
-        # Em caso de erro na requisição, exibe uma mensagem de erro ou log
-        print('Erro ao obter os usuários da API:', e)
-
-        
-
-def get_and_save_livros_from_api():
-    try:
-        response = requests.get('https://cadastro-livros.onrender.com/biblioteca/listar_livros/')
-        response.raise_for_status()  # Verifica se houve algum erro na requisição
-
-        livros_json = response.json()  # Converte a resposta para JSON
-
-        # Para cada livro na resposta da API
-        for livro_data in livros_json:
-            isbn = livro_data.get('isbn')  # Obtenha o ISBN do livro da API
-            # Verifique se o livro já existe no banco de dados pelo ISBN
-            existing_livro = Livro.objects.filter(isbn=isbn).first()
-            if existing_livro:
-                # Se o livro já existir, atualize os detalhes do livro
-                serializer = LivroSerializer(existing_livro, data=livro_data)
-            else:
-                # Se o livro não existir, crie um novo registro
-                serializer = LivroSerializer(data=livro_data)
-
-            if serializer.is_valid():
-                serializer.save()
-    except requests.RequestException as e:
-        # Em caso de erro na requisição, exibe uma mensagem de erro ou log
-        print('Erro ao obter os livros da API:', e)
 
 
 def emprestimos_json(request):
@@ -96,3 +43,58 @@ def emprestimos_json(request):
         for emprestimo in emprestimos
     ]
     return JsonResponse(data, safe=False)
+
+
+class IntegracaoAPIFacade:
+    @staticmethod
+    def obter_e_salvar_usuarios():
+        try:
+            response = requests.get('https://cadastrocliente-production.up.railway.app/api/usuarios')
+            response.raise_for_status()  # Verifica se houve algum erro na requisição
+
+            users_json = response.json()  # Converte a resposta para JSON
+
+            # Para cada usuário na resposta da API
+            for user_data in users_json:
+                cpf = user_data.get('cpf')  # Obtenha o CPF do usuário da API
+                # Verifique se o usuário já existe no banco de dados pelo CPF
+                existing_user = Usuario.objects.filter(cpf=cpf).first()
+                if existing_user:
+                    # Se o usuário já existir, atualize os detalhes do usuário
+                    serializer = UsuarioSerializer(existing_user, data=user_data)
+                else:
+                    # Se o usuário não existir, crie um novo registro
+                    serializer = UsuarioSerializer(data=user_data)
+
+                if serializer.is_valid():
+                    serializer.save()
+        except requests.RequestException as e:
+            # Em caso de erro na requisição, exibe uma mensagem de erro ou log
+            print('Erro ao obter os usuários da API:', e)
+
+
+    @staticmethod
+    def obter_e_salvar_livros():
+        try:
+            response = requests.get('https://cadastro-livros.onrender.com/biblioteca/listar_livros/')
+            response.raise_for_status()  # Verifica se houve algum erro na requisição
+
+            livros_json = response.json()  # Converte a resposta para JSON
+
+            # Para cada livro na resposta da API
+            for livro_data in livros_json:
+                isbn = livro_data.get('isbn')  # Obtenha o ISBN do livro da API
+                # Verifique se o livro já existe no banco de dados pelo ISBN
+                existing_livro = Livro.objects.filter(isbn=isbn).first()
+                if existing_livro:
+                    # Se o livro já existir, atualize os detalhes do livro
+                    serializer = LivroSerializer(existing_livro, data=livro_data)
+                else:
+                    # Se o livro não existir, crie um novo registro
+                    serializer = LivroSerializer(data=livro_data)
+
+                if serializer.is_valid():
+                    serializer.save()
+        except requests.RequestException as e:
+            # Em caso de erro na requisição, exibe uma mensagem de erro ou log
+            print('Erro ao obter os livros da API:', e)
